a1a6a06180feeaec382918d3b34df1082e8a079c
Devin Glispy <glispy.devin@gmail.com>
2021-01-27 17:55:19 -0500

    findGameWinner done and working

--- TODO.md
+++ TODO.md
@@ -3,4 +3,16 @@
 * [x] Command line flags.
 * [x] Handle non-standard teams.
 * [x] Handle errors better.
-  * [ ] Write some tests.
+* [ ] Sort Wins and loses.
+* [ ] Sort Four stocks and "close games" (choosing to arbitrarily define a close game as a game during which each player has at least 2 kills, feel free to change or delete) 
+* [ ] Sort Volleyball games
+* [ ] Delete Trash games
+* [ ] Write some tests.
+* [ ] Personalization options
+* [ ] Create "favorites" list functionality that scans the directory for games against specific users.
+* [ ] Write some more tests.
+* [ ] Handle errors completely in background or as part of app.
+* [ ] Handle single file use case.
+* [ ] Write even more tests.
+* [ ] Perfect commandline version and start on Barebone UI with {"reZach/secure-electron-template"} .
+* [ ] Generate user interface with electron with drag and drop functionality for folders or files.
--- slp_rename.js
+++ slp_rename.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
-const slp = require('slp-parser-js');
-const { default: SlippiGame } = require('slp-parser-js');
+const { default: SlippiGame } = require('@slippi/slippi-js');
+const slp = require('@slippi/slippi-js');
+
 const argv = require('yargs')
 	.usage('Usage $0 [options] <directories>') // Actual assumed syntax> node slp_rename.js "./Folder"
 
@@ -11,16 +12,21 @@ const argv = require('yargs')
 	.describe('n', 'perform a trial run without renaming')
 	.boolean('r')
 	.describe('r', 'rename in subdirectories too')
+	.boolean('s')
+	.describe('s', 'enable sorting')
+	.boolean('f')
+	.describe('f', 'Single file rename')
 	.help('h').argv;
 
 /** Returns character with their tag or color in parentheses (if they have either). */
 function playerName(player, metadata) {
-	const character = slp.characters.getCharacterName(player.characterId);
+	let character = slp.characters.getCharacterName(player.characterId);
 
 	const color = slp.characters.getCharacterColorName(
 		player.characterId,
 		player.characterColor
 	);
+
 	let playerIds = [];
 
 	if (player.nametag) {
@@ -28,19 +34,14 @@ function playerName(player, metadata) {
 	} else if (color !== 'Default') {
 		playerIds.push(color);
 	}
+
 	if (
 		metadata &&
 		metadata.names.netplay !== 'Player' &&
 		metadata.names.netplay !== undefined
 	) {
 		playerIds.push(metadata.names.netplay);
-	}
-
-	//culling long character and player names
-	if (character.length > 5||playerIds.filter(function(x){return x>6;}!=[])) {
-		character = character.slice(0,5);
-		playerIds.map(function(x){return x.slice(0,5)});
-
+		//console.log('Metadata: \n' + metadata);
 	}
 
 	if (playerIds.length > 0) {
@@ -75,7 +76,7 @@ function prettyPrintSingles(settings, metadata) {
 	// kind of annoying that some games don't have metadata
 	let player1, player2;
 	if (metadata) {
-		console.log(metadata);
+		//console.log(metadata);
 		player1 = playerName(settings.players[0], metadata.players[0]);
 		player2 = playerName(settings.players[1], metadata.players[1]);
 	} else {
@@ -115,11 +116,80 @@ function parsedFilename(settings, metadata, file) {
 	return `${datePrefix} - ${pretty}.slp`;
 }
 
+//returns player index of this slp files winner(based on deaths)
+function findGameWinner(file) {
+	/*
+
+	//Get game settings – stage, characters, etc
+	//const settings = file.getSettings();
+	//console.log(settings);
+
+	//Get metadata - start time, platform played on, etc
+	//const metadata = file.getMetadata();
+	//console.log(metadata);
+
+	//Get computed stats - openings / kill, conversions, etc
+	//const stats = file.getStats();
+	//console.log('Playerindex 0 kills:' + stats.overall[0].killCount);
+	//console.log('Playerindex 1 kills:' + stats.overall[1].killCount);
+	
+	//Get an ordered list of each stock taken
+	//const stonks = file.stocks;
+	//console.log(stonks[stonks.length-1]);
+	
+	*/
+
+	const stats = file.getStats(); //get computed stats - openings / kill, conversions, etc
+	const stonks = stats.stocks; //get an ordered list of each stock taken
+	const laststonk = stonks[stonks.length - 1]; //get last stock that ended
+	if (laststonk.count == 1){
+		//If this was their last stock the game is over and the otherperson wins
+		console.log(
+			"Winner's playerIndex is " + laststonk.opponentIndex + ' by stock elim'
+		);
+		return laststonk.opponentIndex;
+	} else {
+		//we only calc the special case on call for effiency
+		let killLeader = 0;
+		let second_best = null;
+		for (let i = 1; i <= stats.length; i++) {
+			//check port1 against everyother port for who has the most kills
+			if (stats.overall[killLeader].killCount < stats.overall[i].killCount)
+				killLeader = second_best;
+			killLeader = i;
+		}
+		if (second_best == null) {
+			console.log(
+				"Winner's playerIndex is: " + killLeader + ' via becoming kill leader'
+			);
+			return killLeader;
+		} else {
+			console.log('no fucking idea who won... you choose: \n' + stats.overall);
+		}
+	}
+	//Error, Tie, or Timeout
+	return -1;
+}
+
 function isDirectory(dir) {
 	const stats = fs.lstatSync(dir);
 	return stats && stats.isDirectory();
 }
 
+function isFile(file) {
+	return err('TODO: implement');
+}
+
+function sortFiles(safe, dir) {
+	if (!fs.existsSync((dir + '/Wins').join())) {
+		try {
+			fs.open(dir);
+		} catch {}
+	}
+	return err("Couldn't sort, my b");
+}
+
+//MULTIFILE ALGORITHM
 const directories = argv._;
 
 while (directories.length > 0) {
@@ -134,16 +204,29 @@ while (directories.length > 0) {
 
 	const files = fs.readdirSync(dir);
 	for (const file of files) {
+		//MULTIFILE ALGO ONLY
+		console.log('\n\n\nCurrent Game:' + file);
+		console.log('Loading...');
+		//SINGLE-FILE ALGO
+
 		const filePath = path.join(dir, file);
 		if (argv.r && isDirectory(filePath)) {
 			directories.push(filePath);
+			console.log(
+				'Oops ' +
+					file +
+					' was a folder. Adding the contents to the end of the current list...'
+			);
 			continue;
-		} else if (!file.match('.slp$')) {
-			console.log(`'${file}' skipped.`);
-			continue;
+		} else {
+			if (!file.match(/(^.*)(.slp$)/)) {
+				console.log(`'${file}' skipped.`);
+				continue;
+			}
 		}
-
 		const game = new SlippiGame(filePath);
+
+		findGameWinner(game);
 		const settings = game.getSettings();
 		const metadata = game.getMetadata();
 
